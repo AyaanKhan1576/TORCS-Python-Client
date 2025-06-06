3fcbd9628dfafd7f45981ceba4a03822e5d62dac
Ayaan Khan <khanayaan.2003@gmail.com>
2025-06-07 01:05:31 +0500

    Cleaning repo

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-bad = pd.read_csv("G-Speedway_Peugeot-406_01.csv")
-bad = bad.replace("unknown", pd.NA)
-missing = bad[ALL_NUM + TARGETS].isna().all()
-print("Entire columns that are NaN:", missing[missing].index.tolist())
